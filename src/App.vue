6ed7f05abddc230a0eb5a5bc2e22915af19f7324
shady356 <heenriko@gmail.com>
2021-06-27 00:22:49 +0200

    feat: set main navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/my-work">My Work</router-link> |
-      <router-link to="/contact">Contact</router-link>
-    </div>
+    <MainNavigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import MainNavigation from '@/components/global/MainNavigation.vue'
+export default {
+  name: 'App',
+  components: {
+    MainNavigation
+  }
+}
+</script>
 
 <style lang="scss">
   * {
     margin: 0;
     padding: 0;
+  }
+  body {
+    background: $blue;
   }
 </style>
---
+++ src/components/global/MainNavigation.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav class="main-navigation-container">
+    <ul class="link-list">
+      <li class="link-item" v-for="link in links" :key="link.name">
+        <router-link :to="link.path">
+          {{ link.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainNavigation',
+  data() {
+    return {
+      links: [
+        {
+          name: 'Home',
+          path: '/'
+        },
+        {
+          name: 'About',
+          path: '/about'
+        },
+        {
+          name: 'My Work',
+          path: '/my-work'
+        },
+        {
+          name: 'Contact',
+          path: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-navigation-container {
+  position: sticky;
+  top: $space-m;
+  left: 100%;
+  padding-right: $space-xl;
+  display: inline-block;
+  right: 100px;
+
+  .link-list {
+    display: flex;
+    justify-content: flex-end;
+    list-style: none;
+    
+    .link-item {
+      a {
+        color: $white-blue;
+        text-decoration: none;
+        padding: $space-xs $space-m;
+
+        :active {
+          background: $white-blue;
+          color: $blue;
+        } 
+      }
+    }
+  }
+}
+</style>
