4af1c4079c3237efb9d0354d31139332f7230e22
shady356 <heenriko@gmail.com>
2021-06-26 22:34:57 +0200

    feat: routes and pages added

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/my-work',
+    name: 'MyWork',
+    component: () => import(/* webpackChunkName: "myWork" */ '../views/MyWork.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
   }
 ]
 
---
+++ src/views/MyWork.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>My Work</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyWork'
+}
+</script>
